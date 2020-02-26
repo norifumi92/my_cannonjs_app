8c08bbb792d5043ba748bfb73821018aa01c7937
Norifumi Irie <norifumi.irie@tyrellsys.com.my>
2020-02-26 18:05:14 +0800

    Cleared createMeshes()

--- main.js
+++ main.js
@@ -80,45 +80,6 @@ function createGroundMass() {
 //Configure meshes
 function createMeshes() {
 
-    // create a Group to hold the pieces of the train
-    train = new THREE.Group();
-
-    const materials = createMaterials();
-    const geometries = createGeometries();
-    const nose = new THREE.Mesh( geometries.nose, materials.body );
-    nose.rotation.z = Math.PI / 2;
-    nose.position.x = -1;
-
-    const cabin = new THREE.Mesh( geometries.cabin, materials.body );
-    cabin.position.set( 1.5, 0.4, 0 );
-
-    const chimney = new THREE.Mesh( geometries.chimney, materials.detail );
-    chimney.position.set( -2, 0.9, 0);
-
-    const smallWheelRear = new THREE.Mesh( geometries.wheel, materials.detail );
-    smallWheelRear.position.set( 0, -0.5, 0 );
-  
-    const smallWheelCenter = smallWheelRear.clone();
-    smallWheelCenter.position.x = -1;
-
-    const smallWheelFront = smallWheelRear.clone();
-    smallWheelFront.position.x = -2;
-
-    const bigWheel = smallWheelRear.clone();
-    bigWheel.scale.set( 2, 2, 1.25 );
-    bigWheel.position.set( 1.5, -0.1, 0 );
-
-    train.add( 
-        nose, 
-        cabin,
-        chimney,
-        smallWheelRear,
-        smallWheelCenter,
-        smallWheelFront,
-        bigWheel,
-        )
-    
-    scene.add( train );
 }
 
 // Configure renderer and set it into container
